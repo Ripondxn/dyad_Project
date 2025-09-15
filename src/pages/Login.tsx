ee2c3ec1a77809288d400384776a0a3679f61f9b
[dyad] <ripondxn@gmail.com>
2025-09-15 15:55:12 +0400

    [dyad] Re-enabling social login providers - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -50,7 +50,7 @@ const Login = () => {
           <Auth
             supabaseClient={supabase}
             appearance={{ theme: ThemeSupa }}
-            providers={[]}
+            providers={['google', 'github']}
             theme="light"
             redirectTo={`${window.location.origin}/`}
           />
